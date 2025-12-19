150f2769a0a84d7e522015ec0089ff6c0ef54e35
Scander Mustapha <mustapha.ziy@gmail.com>
2025-12-19 09:05:34 +0100

    fix test deps

--- test/test.ml
+++ test/test.ml
@@ -1,4 +1,4 @@
 let () = SwfParser.init SwfZip.inflate SwfZip.deflate
 let _header, _tags =
-  SwfParser.parse (IO.input_channel (In_channel.open_bin "test/1012.swf"))
+  SwfParser.parse (IO.input_channel (In_channel.open_bin "./1012.swf"))
 
